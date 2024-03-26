ecad0f1b6bfe314768f6908bb6c318b935c029e9
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-03-26 20:45:05 -0300

    .

--- routes/clientes.js
+++ routes/clientes.js
@@ -3,22 +3,22 @@ const router = express.Router();
 
 /* GET clientes listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource GET');
+    res.send('respond with a resource GET');
 });
 
 /* PUT clientes listing. */
 router.put('/', function(req, res, next) {
-  res.send('respond with a resource PUT');
+    res.send('respond with a resource PUT');
 });
 
 /* POST clientes listing. */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource POST');
+    res.send('respond with a resource POST');
 });
 
 /* DELETE clientes listing. */
 router.delete('/', function(req, res, next) {
-  res.send('respond with a resource DELETE');
+    res.send('respond with a resource DELETE');
 });
 
 module.exports = router;
--- routes/produtos.js
+++ routes/produtos.js
@@ -3,22 +3,22 @@ const router = express.Router();
 
 /* GET produtos listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource GET');
+    res.send('respond with a resource GET');
 });
 
 /* PUT produtos listing. */
 router.put('/', function(req, res, next) {
-  res.send('respond with a resource PUT');
+    res.send('respond with a resource PUT');
 });
 
 /* POST produtos listing. */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource POST');
+    res.send('respond with a resource POST');
 });
 
 /* DELETE produtos listing. */
 router.delete('/', function(req, res, next) {
-  res.send('respond with a resource DELETE');
+    res.send('respond with a resource DELETE');
 });
 
 module.exports = router;
