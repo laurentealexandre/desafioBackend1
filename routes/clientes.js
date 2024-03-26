34a5c15c58ed3c9bfe51694815679f85c12cea36
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-03-26 15:29:30 -0300

    .

---
+++ routes/clientes.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET clientes listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource GET');
+    });
+
+/* PUT clientes listing. */
+router.put('/', function(req, res, next) {
+    res.send('respond with a resource PUT');
+    });
+
+  /* POST clientes listing. */
+router.post('/', function(req, res, next) {
+    res.send('respond with a resource POST');
+    });
+
+/* DELETE clientes listing. */
+router.delete('/', function(req, res, next) {
+    res.send('respond with a resource DELETE');
+    });
+
+module.exports = router;
