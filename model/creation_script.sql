8ded0afd04c139f16bd0df58237b4cd5e4315aa6
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-03-29 19:45:35 -0300

    mmm

--- model/creation_script.sql
+++ model/creation_script.sql
@@ -1,3 +1,17 @@
-CREATE TABLE `desafio`.`clientes` (`ID` INT(10) NULL AUTO_INCREMENT , `Nome` VARCHAR(255) NOT NULL , `Sobrenome` VARCHAR(255) NOT NULL , `Email` VARCHAR(255) NOT NULL , `Idade` INT(3) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB; 
+CREATE TABLE `desafio`.`clientes` (
+    `ID` INT(10) NULL AUTO_INCREMENT,
+    `Nome` VARCHAR(255) NOT NULL,
+    `Sobrenome` VARCHAR(255) NOT NULL,
+    `Email` VARCHAR(255) NOT NULL,
+    `Idade` INT(3) NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE = InnoDB;
 
-CREATE TABLE `desafio`.`produtos` (`ID` INT(10) NULL AUTO_INCREMENT , `Nome` VARCHAR(255) NOT NULL , `Descricao` VARCHAR(255) NOT NULL , `preco` FLOAT(10) NOT NULL , `data_atualizado` DATE NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB; 
+CREATE TABLE `desafio`.`produtos` (
+    `ID` INT(10) NULL AUTO_INCREMENT,
+    `Nome` VARCHAR(255) NOT NULL,
+    `Descricao` VARCHAR(255) NOT NULL,
+    `preco` FLOAT(10) NOT NULL,
+    `data_atualizado` DATE NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE = InnoDB;
