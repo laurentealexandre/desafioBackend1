7e154e8fb883a78f66c0d818494d1e36fe29b649
Alexandre Laurente Cardoso <laurentealexandre@gmail.com>
2024-04-01 15:07:44 -0300

    ,

---
+++ model/insert_script.sql
@@ -0,0 +1,17 @@
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('Gustavo','Vale','gustavo@email.com','20');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('Maria','Santos','maria@email.com','33');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('José','Silva','jose@email.com','25');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('João','Santos','joao@email.com','19');
+
+INSERT INTO `produtos`(`nome`, `Descricao`, `preco`, `data_atualizado`) VALUES
+('Notebook','Dell','2500','22024-04-01 14:32:42');
+INSERT INTO `produtos`(`nome`, `Descricao`, `preco`, `data_atualizado`) VALUES
+('Pendrive','16gb','40','2024-04-01 14:32:57');
+INSERT INTO `produtos`(`nome`, `Descricao`, `preco`, `data_atualizado`) VALUES
+('Mouse','Sem fio','150','2024-04-01 14:33:11');
+INSERT INTO `produtos`(`nome`, `Descricao`, `preco`, `data_atualizado`) VALUES
+('Teclado','Com fio','100','2024-04-01 14:33:23');
